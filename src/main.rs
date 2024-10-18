9522dee5f028849f1e5c0a72d81df3ff3f496ecf
kdonthi <kaushikdr@gmail.com>
2024-10-18 10:23:13 -0700

    get tx from endpoint and make `DataItem`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::ffi::CString;
 use std::future::Future;
 use std::io::Cursor;
 use std::option::Option;
@@ -9,11 +10,18 @@ use base64::{decode_config, URL_SAFE};
 
 
 #[derive(Deserialize, Debug)]
-struct Bundle {
+struct Tag {
+    name: String,
+    value: String
+}
+
+#[derive(Deserialize, Debug)]
+struct Transaction {
     format: i32,
     id: String,
     last_tx: String,
     owner: String,
+    tags: Vec<Tag>,
     target: String,
     quantity: String,
     data: String,
@@ -37,13 +45,71 @@ struct Offset {
     offset: String,
 }
 
-struct Peers {
+#[derive(Deserialize, Debug)]
+struct AvroField {
+    name: String,
+    #[serde(alias = "type")]
+    typ: String
+}
 
+#[derive(Deserialize, Debug)]
+struct AvroItems {
+    #[serde(alias = "type")]
+    typ: String,
+    name: String,
+    fields: Vec<AvroField>
+}
+
+#[derive(Deserialize, Debug)]
+struct AvroArray {
+    #[serde(alias = "type")]
+    typ: String,
+    items: AvroItems
+}
+
+#[derive(Deserialize, Debug)]
+struct DataItem {
+    signature_type: String,
+    signature: String,
+    owner: String,
+    target: String,
+    anchor: String,
+    tag_count: i32,
+    tag_bytes: i32,
+    tags: AvroArray,
+    data: Vec<u8>
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let id = "F8Bcp5-dfOhRnOZm7dev58uRKmiXkn9my4d6WnkyPDU";
+
+    let tx = reqwest::get(format!("https://arweave.net/tx/{}", id))
+        .await
+        .unwrap()
+        .json::<Transaction>()
+        .await
+        .unwrap();
+
+    let mut seen_bundle_format = false;
+    let mut seen_bundle_version = false;
+
+    for tag in tx.tags {
+        let name_bytes = decode_config(tag.name, URL_SAFE).unwrap();
+        let name = String::from_utf8(name_bytes).unwrap();
+
+        if name == "Bundle-Format" {
+            seen_bundle_format = true;
+        }
+        if name == "Bundle-Version" {
+            seen_bundle_version = true;
+        }
+    }
+
+    if !seen_bundle_version || !seen_bundle_format {
+        panic!("transaction id given is not a bundle");
+    }
+
     let offset = reqwest::get(format!("https://arweave.net/tx/{}/offset", id))
         .await
         .unwrap()
@@ -53,19 +119,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut offset_size = atoi::<i64>(offset.offset.as_bytes()).unwrap();
     let mut tx_size = atoi::<i64>(offset.size.as_bytes()).unwrap();
-
     let chunk_size = 256 * 1024;
-    let mut known_peers: Vec<String> = Vec::new();
-    known_peers.push(String::from("https://arweave.net"));
 
     let mut b: Vec<u8> = Vec::new();
     while tx_size > 0 {
         println!("offset size {} tx left {} chunk size {}", offset_size, tx_size, chunk_size);
-        let resp: Response = reqwest::get(format!("https://arweave.net/chunk/{}", offset_size)).await?;
+        let resp: Response = reqwest::get(format!("https://arweave.net/chunk/{}", offset_size)).await?; //  TODO ALL THE TRANSACTIONS ARE JUST STORED IN THIS HUGE CHUNK ??????????
         let json_chunk: Chunk = match resp.json::<Chunk>().await {
             Ok(x) => x,
             Err(_e) => {
-                panic!("{}", format!("could not find chunk {}", offset_size))
+                panic!("{}", format!("could not find chunk for offset {}", offset_size))
             },
         };
 
@@ -80,8 +143,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         tx_size -= chunk_size
     }
 
-    // Assume chunk is in text format
-    // const chunk_size = Bytes::from(chunk);
     Ok(())
 }
 
